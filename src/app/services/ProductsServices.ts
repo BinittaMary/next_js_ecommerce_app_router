d6c9c021b3bde58521eb913f29cafaeb8f706964
binitta_chandy <binittamary.chandy@gmail.com>
2026-05-19 12:36:32 +0400

    Product Page fix

--- src/app/services/ProductsServices.ts
+++ src/app/services/ProductsServices.ts
@@ -1,6 +1,11 @@
 export async function getProducts() {
-const res= await fetch('https://fakestoreapi.com/products',{
-  cache :"no-store"
-});
-return res.json();
+  const res = await fetch("https://fakestoreapi.com/products", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch products");
+  }
+
+  return await res.json();
 }
