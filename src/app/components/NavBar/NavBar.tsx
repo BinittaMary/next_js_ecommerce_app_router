18721264626224f6d18d17265b989f6e22698041
binitta_chandy <binittamary.chandy@gmail.com>
2026-05-19 18:29:01 +0400

    Product Page fix 4

--- src/app/components/NavBar/NavBar.tsx
+++ src/app/components/NavBar/NavBar.tsx
@@ -8,7 +8,7 @@ function NavBar() {
   const pathname = usePathname();  
   return (        
         <div>   
-            <nav className="navbar navbar-expand-lg navbar-bg-color navbar-dark">
+            <nav className="navbar navbar-expand-lg navbar-bg-color navbar-dark" style={{backgroundColor:"#a39a4e"}} >
             <div className="container-fluid">
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
